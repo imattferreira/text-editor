ae5bb159ca31b7814e81b050955775a8c96e4c43
Matheus Ferreira <mattferreira.dev@protonmail.com>
2024-06-12 22:05:41 -0300

    feat: create `TextNode`

---
+++ src/text-editor/nodes/text-node.ts
@@ -0,0 +1,52 @@
+import Node, { JsonNode } from "./node";
+
+type TextNodeData = {
+  text: string;
+};
+
+class TextNode extends Node<TextNodeData> {
+  constructor(text: string) {
+    super({ text });
+  }
+
+  static getType(): string {
+    return "text";
+  }
+
+  static fromJson(node: JsonNode<TextNodeData>): TextNode {
+    return TextNode.create(node.data.text);
+  }
+
+  static fromHtml(node: HTMLElement): TextNode {
+    return TextNode.create(node.childNodes[0].textContent || "");
+  }
+
+  static create(text: string) {
+    return new TextNode(text);
+  }
+
+  toHtml(): HTMLElement {
+    const el = document.createElement("p");
+    const text = document.createTextNode(this.data.text);
+
+    el.appendChild(text);
+
+    return el;
+  }
+
+  toJson(): JsonNode {
+    return {
+      type: TextNode.getType(),
+      nodes: [],
+      data: {
+        text: this.data.text,
+      },
+    };
+  }
+
+  toText(): string {
+    return this.data.text;
+  }
+}
+
+export default TextNode;
